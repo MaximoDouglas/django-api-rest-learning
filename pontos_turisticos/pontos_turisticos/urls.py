72e49d9bd74a2bba005d70b0fdc9bb514c9502eb
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-03-21 14:53:30 -0300

    origin/master Add endpoints to project urls
    - Add atracoes endpoint
    - Add comentarios endpoint
    - Add enderecos endpoint
    - Add avaliacoes endpoint
    
    Signed-off-by: MaximoDouglas <douglas.h.maximo@gmail.com>

--- pontos_turisticos/pontos_turisticos/urls.py
+++ pontos_turisticos/pontos_turisticos/urls.py
@@ -16,11 +16,19 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from rest_framework import routers
-from core.api.viewsets import PontoTuristicoViewSet
 from django.conf.urls import include
+from core.api.viewsets import PontoTuristicoViewSet
+from atracoes.api.viewsets import AtracaoViewSet
+from enderecos.api.viewsets import EnderecoViewSet
+from comentarios.api.viewsets import ComentarioViewSet
+from avaliacoes.api.viewsets import AvaliacaoViewSet
 
 router = routers.DefaultRouter()
 router.register(r'pontosturisticos', PontoTuristicoViewSet)
+router.register(r'atracoes', AtracaoViewSet)
+router.register(r'enderecos', EnderecoViewSet)
+router.register(r'comentarios', ComentarioViewSet)
+router.register(r'avaliacoes', AvaliacaoViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
