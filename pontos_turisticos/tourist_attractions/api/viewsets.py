74438e51df0d038cfb8075cf9458ae95fe3b514a
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-03-22 22:47:50 -0300

    origin/master Add query filters
    - Add optional query filter on touristic_points to filter by (name or id) and description
    
    Signed-off-by: MaximoDouglas <douglas.h.maximo@gmail.com>

--- pontos_turisticos/tourist_attractions/api/viewsets.py
+++ pontos_turisticos/tourist_attractions/api/viewsets.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from django.db.models import Q
 from tourist_attractions.models import TouristAttraction
 from tourist_attractions.api.serializers import TouristAttractionSerializer
 from rest_framework.response import Response
@@ -8,7 +9,22 @@ class TouristAttractionViewSet(ModelViewSet):
     serializer_class = TouristAttractionSerializer
 
     def get_queryset(self):
-        return TouristAttraction.objects.filter(approved=True)
+        id          = self.request.query_params.get('id', None)
+        name        = self.request.query_params.get('name', None)
+        description = self.request.query_params.get('description', None)
+
+        queryset = TouristAttraction.objects.all()
+
+        if (id and name):
+            id_query                    = Q(pk=id)
+            name_query_case_insensitive = Q(name__iexact=name)
+
+            queryset = queryset.filter(id_query | name_query_case_insensitive)
+
+        if description:
+            queryset = queryset.filter(description=description)
+
+        return queryset
 
     def list(self, request, *args, **kwargs):
         return super(TouristAttractionViewSet, self).list(request, *args, **kwargs)
