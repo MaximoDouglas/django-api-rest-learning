916b75d61f229e36bc09ce52b202a24b201c9766
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-03-24 20:38:48 -0300

    origin/master Add nested serializers to tourist_attractions app
    - Add 'shows' and 'address' nested serializer to the Tourist Attractions Serializer
    
    Signed-off-by: MaximoDouglas <douglas.h.maximo@gmail.com>

--- pontos_turisticos/tourist_attractions/api/serializers.py
+++ pontos_turisticos/tourist_attractions/api/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework.serializers import ModelSerializer
 from tourist_attractions.models import TouristAttraction
+from shows.api.serializers import ShowSerializer
+from adresses.api.serializers import AddressSerializer
 
 class TouristAttractionSerializer(ModelSerializer):
+    shows   = ShowSerializer(many=True)
+    address = AddressSerializer()
+    
     class Meta:
         model  = TouristAttraction
         fields = '__all__'
